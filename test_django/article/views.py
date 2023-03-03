6f2d9d62bcc09775859c6b0af0f7fc53adf278a4
muztishini <a.kroshakov@yandex.ru>
2023-03-03 13:27:49 +0500

    Initial commit

---
+++ test_django/article/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+
+Простой ViewSet, предназначенный для перечисления или поиска пользователей.
+    """
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
