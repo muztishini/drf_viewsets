23716d36741412778431e8bdbad50fd4c5e85094
muztishini <a.kroshakov@yandex.ru>
2023-03-06 14:23:59 +0500

    add postgresql

--- test_django/test_django/settings.py
+++ test_django/test_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'test_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testpostsql',
+        'USER': 'testuser',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1'
     }
 }
 
