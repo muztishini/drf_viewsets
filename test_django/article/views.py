2bfe68b306a02cceebd9a5d675421ab85262b441
muztishini <111636144+muztishini@users.noreply.github.com>
2023-03-16 13:32:56 +0500

    new commit

--- test_django/article/views.py
+++ test_django/article/views.py
@@ -1,12 +1,10 @@
 from rest_framework import viewsets
-
 from .models import Article
 from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     """
-
 Простой ViewSet, предназначенный для перечисления или поиска пользователей.
     """
     serializer_class = ArticleSerializer
