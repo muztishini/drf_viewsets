a825b5237223871626487c7c906f9d8dda3ac0ec
muztishini <111636144+muztishini@users.noreply.github.com>
2023-03-23 15:31:45 +0500

    small changes

--- test_django/article/serializers.py
+++ test_django/article/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    # author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Article
         fields = ("id", "title", "description", "body", "author")
+
+    # def create(self, validated_data):
+    #     author = validated_data.pop('author')
+    #     article = Article.objects.create(**validated_data)
+    #     Author.objects.create(author=author, article=article)
+    #     return article
--- test_django/test_django/settings.py
+++ test_django/test_django/settings.py
@@ -76,16 +76,22 @@ WSGI_APPLICATION = 'test_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'testpostsql',
+#         'USER': 'testuser',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testpostsql',
-        'USER': 'testuser',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
